296ab9ad0f1b0bab0114096973df0c069455c166
Marcus Luk <marcus.luk@oocl.com>
2020-11-26 14:14:29 +0800

    [TDD-Practice][Marcus Luk] feat: Used constant for output strings.

--- src/main/java/tdd/fizzbuzz/FizzBuzz.java
+++ src/main/java/tdd/fizzbuzz/FizzBuzz.java
@@ -1,19 +1,24 @@
 package tdd.fizzbuzz;
 
+
 public class FizzBuzz {
+    private final String FIZZ = "Fizz";
+    private final String BUZZ = "Buzz";
+    private final String WHIZZ = "Whizz";
+
     public String countOff(int number) {
         String result = "";
 
         if (number % 3 == 0) {
-            result += "Fizz";
+            result += FIZZ;
         }
 
         if (number % 5 == 0) {
-            result += "Buzz";
+            result += BUZZ;
         }
 
         if (number % 7 == 0) {
-            result += "Whizz";
+            result += WHIZZ;
         }
 
         if (result.equals("")) {
