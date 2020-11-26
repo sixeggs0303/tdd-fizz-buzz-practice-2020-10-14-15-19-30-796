79d5d19cdcc77d30e1e33bfde5af39232b0e5ffc
Marcus Luk <marcus.luk@oocl.com>
2020-11-26 14:04:35 +0800

    [TDD-Practice][Marcus Luk] feat: Refactor for result output.

--- src/main/java/tdd/fizzbuzz/FizzBuzz.java
+++ src/main/java/tdd/fizzbuzz/FizzBuzz.java
@@ -2,10 +2,15 @@ package tdd.fizzbuzz;
 
 public class FizzBuzz {
     public String countOff(int number) {
+        String result = "";
+
         if (number % 3 == 0) {
-            return "Fizz";
+            result += "Fizz";
         }
 
-        return String.valueOf(number);
+        if (result.equals("")) {
+            return String.valueOf(number);
+        }
+        return result;
     }
 }
