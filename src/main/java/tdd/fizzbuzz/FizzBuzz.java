749bfac1eb5bcfd1dcc13ec1badc312a0e90db17
Marcus Luk <marcus.luk@oocl.com>
2020-11-26 14:02:54 +0800

    [TDD-Practice][Marcus Luk] feat: Passed test of Fizz case "3".

--- src/main/java/tdd/fizzbuzz/FizzBuzz.java
+++ src/main/java/tdd/fizzbuzz/FizzBuzz.java
@@ -1,7 +1,11 @@
 package tdd.fizzbuzz;
 
 public class FizzBuzz {
-    public String countOff(int number){
+    public String countOff(int number) {
+        if (number % 3 == 0) {
+            return "Fizz";
+        }
+
         return String.valueOf(number);
     }
 }
