533ccbf84ab65059efd20833987a16c66ba76222
Marcus Luk <marcus.luk@oocl.com>
2020-11-26 13:57:20 +0800

    [TDD-Practice][Marcus Luk] feat: Failed test of normal case "1".

--- src/test/java/tdd/fizzbuzz/FizzBuzzTest.java
+++ src/test/java/tdd/fizzbuzz/FizzBuzzTest.java
@@ -1,4 +1,20 @@
 package tdd.fizzbuzz;
 
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class FizzBuzzTest {
+    @Test
+    void should_return_1_when_count_off_given_order_number_1() {
+        // given
+        int order = 1;
+        FizzBuzz fizzBuzz = new FizzBuzz();
+
+        // when
+        String actual = fizzBuzz.countOff(order);
+
+        // then
+        assertEquals("1", actual);
+    }
 }
